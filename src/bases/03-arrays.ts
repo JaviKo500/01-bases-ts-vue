55bf31beed98166039ad08f9e32fc8ef81e2bf97
JaViKo500 <javikogutierrez64@gmail.com>
2024-12-03 22:50:03 -0500

    arrays

---
+++ src/bases/03-arrays.ts
@@ -0,0 +1,12 @@
+export const numbersArray = [1, 2, 3, 4, 5];
+
+
+numbersArray.push(6)
+console.log('<--------------- JK 03-arrays --------------->');
+console.log(numbersArray);
+
+const numbersArray2: (number  )[] = [...numbersArray,];
+
+numbersArray2.push(7);
+console.log('<--------------- JK 03-arrays --------------->');
+console.log(numbersArray2);
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import './bases/02-object';
+import './bases/03-arrays';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
