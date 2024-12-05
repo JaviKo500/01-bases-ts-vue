a02369beb274c2e8df22c5a3c6147d8f02e33a91
JaViKo500 <javikogutierrez64@gmail.com>
2024-12-04 23:45:36 -0500

    typed data

--- src/bases/06-imp-exp.ts
+++ src/bases/06-imp-exp.ts
@@ -1,9 +1,9 @@
-import heroes, { owners } from "./data/heroes";
+import heroes, { type Owner, owners, type Hero } from "./data/heroes";
 
 console.log('<--------------- JK 06-imp-exp --------------->');
 console.log(owners, heroes);
 
-export const getHeroById = (id: number) => {
+export const getHeroById = (id: number): Hero => {
    const hero = heroes.find(hero => hero.id === id);
    if (!hero) {
       throw new Error(`No hero found with id ${id}`);
@@ -14,7 +14,7 @@ export const getHeroById = (id: number) => {
 console.log('<--------------- JK 06-imp-exp --------------->');
 console.log(getHeroById(1));
 
-export const getHeroesByOwner = (owner: 'DC' | 'Marvel') => {
+export const getHeroesByOwner = (owner: Owner) => {
    const heroesOwner = heroes.filter(hero => hero.owner.toLowerCase() === owner.toLowerCase());
    if (heroes.length === 0) {
       throw new Error(`No heroes found with owner ${owner}`);
