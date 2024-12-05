56a3ec5eee3827cfc5c2bef2954bf15cba9092c2
JaViKo500 <javikogutierrez64@gmail.com>
2024-12-04 23:38:54 -0500

    export e import

---
+++ src/bases/06-imp-exp.ts
@@ -0,0 +1,28 @@
+import heroes, { owners } from "./data/heroes";
+
+console.log('<--------------- JK 06-imp-exp --------------->');
+console.log(owners, heroes);
+
+export const getHeroById = (id: number) => {
+   const hero = heroes.find(hero => hero.id === id);
+   if (!hero) {
+      throw new Error(`No hero found with id ${id}`);
+   }
+   return hero;
+}
+
+console.log('<--------------- JK 06-imp-exp --------------->');
+console.log(getHeroById(1));
+
+export const getHeroesByOwner = (owner: 'DC' | 'Marvel') => {
+   const heroesOwner = heroes.filter(hero => hero.owner.toLowerCase() === owner.toLowerCase());
+   if (heroes.length === 0) {
+      throw new Error(`No heroes found with owner ${owner}`);
+   }
+   return heroesOwner;
+   
+}
+
+console.log('<--------------- JK 06-imp-exp --------------->');
+console.log(getHeroesByOwner('DC'));
+console.log(getHeroesByOwner('Marvel'));
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import './bases/05-destructuring';
+import './bases/06-imp-exp';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
