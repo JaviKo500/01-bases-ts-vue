359607c31370a20241c54250144f623e2b256531
JaViKo500 <javikogutierrez64@gmail.com>
2024-12-16 22:28:08 -0500

    promises with args

--- src/bases/08-promises.ts
+++ src/bases/08-promises.ts
@@ -1,16 +1,34 @@
-console.log('<--------------- JK 08-promises --------------->');
-console.log('init');
+// console.log('<--------------- JK 08-promises --------------->');
+// console.log('init');
 
-new Promise((resolve, reject) => {
-    setTimeout(() => {
-      //   resolve('resolved');
-        reject('rejected');
-    }, 1000);
-}).then((value) => {
-    console.log(value);
+import { getHeroById } from "./06-imp-exp";
+import { Hero } from "./data/heroes";
+
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       //   resolve('resolved');
+//         reject('rejected');
+//     }, 1000);
+// }).then((value) => {
+//     console.log(value);
+// }).catch((error) => {
+//     console.log(error);
+// }).finally(() => {
+//     console.log('finally');
+// });
+// console.log('end');
+
+const getHeroByIdAsync = (id: number): Promise<Hero> => {
+   return new Promise((resolve, reject) => {
+      setTimeout(() => {
+         const hero = getHeroById(id);
+         return hero ? resolve(hero) : reject('No hero found');
+      }, 1000);
+   });
+}
+
+getHeroByIdAsync(10).then((hero) => {
+   console.log(hero);
 }).catch((error) => {
-    console.log(error);
-}).finally(() => {
-    console.log('finally');
+   console.log(error);
 });
-console.log('end');
